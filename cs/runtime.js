88fa7362316e616ee65f32c9dd68099a9fa3ae16
Clayton  J. Wilkerson <clay2000lbs@gmail.com>
2021-11-28 21:18:29 -0600

    part 1 done

--- cs/runtime.js
+++ cs/runtime.js
@@ -47,16 +47,20 @@ const extraLargeArray = getSizedArray(100000);
 
 // Try it with first function
 perf.start();                     // Starts timer
-doublerAppend(extraLargeArray);
+doublerAppend(largeArray);
 let resultsAppend = perf.stop();  // Stops timer and save time results
 
 
 // Try it with second function
 perf.start();
-doublerInsert(extraLargeArray);
+doublerInsert(largeArray);
 let resultsInsert = perf.stop();
 
 
 console.log('Results for the extraLargeArray');
 console.log("insert", resultsInsert.preciseWords);
 console.log("append", resultsAppend.preciseWords);
+
+// For the tiny and small array, the insert method is faster; I assume that the process of shifting the position of each index is faster than
+// the process of appending to the end of an array at smaller array lengths. The append method seems to scale better since it is faster with larger array sizes. 
+// The difference in speed at lower array sizes is not significant enough to warrant using unshift when looking to scale.
